fbcf0a5ac18dc6720ae85eba8f3c09d89d0be4fb
DEEPAK PRIME <pranesha182004@gmail.com>
2024-06-01 19:29:00 +0530

    firstchange

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    const [value,setValue]=useState("")
+    return (
+        <div className='calculator-grid'>
+           <div className='userinput'>{value}</div>  
+           <button className='special' onClick={e=>setValue("")}>AC</button>
+           <button className='special' onClick={e=>setValue(value.slice(0,-1))}>DE</button>
+           <button className='special'  value="%" onClick={e=>setValue(value+e.target.value)}>%</button>
+           <button className='special' value="/" onClick={e=>setValue(value+e.target.value)}>/</button>
+           <button value="7" onClick={e=>setValue(value+e.target.value)}>7</button>
+           <button value="8" onClick={e=>setValue(value+e.target.value)}>8</button>
+           <button value="9" onClick={e=>setValue(value+e.target.value)}>9</button>
+           <button className='special' value="*" onClick={e=>setValue(value+e.target.value)}>*</button>
+           <button value="4" onClick={e=>setValue(value+e.target.value)}>4</button>
+           <button value="5" onClick={e=>setValue(value+e.target.value)}>5</button>
+           <button value="6" onClick={e=>setValue(value+e.target.value)}>6</button>
+           <button className='special' value="-" onClick={e=>setValue(value+e.target.value)}>-</button>
+           <button value="1" onClick={e=>setValue(value+e.target.value)}>1</button>
+           <button value="2" onClick={e=>setValue(value+e.target.value)}>2</button>
+           <button value="3" onClick={e=>setValue(value+e.target.value)}>3</button>
+           <button className='special' value="+" onClick={e=>setValue(value+e.target.value)}>+</button>
+           <button value="0" onClick={e=>setValue(value+e.target.value)}>0</button>
+           <button value="." onClick={e=>setValue(value+e.target.value)}>.</button>
+           <button value="^" onClick={e=>setValue(value+e.target.value)}>^</button>
+           <button className="equals" onClick={e=>setValue(eval(value))}>=</button>
+           
+                        
+        </div>  
+    );
 }
 export default App;
